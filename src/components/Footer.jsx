0f6173675ae31e23ddd49dbbb03429acb175672c
Daniel Reis <ndanielsouzareis@gmail.com>
2026-04-25 18:44:24 -0300

    alterecao no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../styles/footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 - Este site é um projeto independente para fins educacionais e não possui vínculo oficial com o SENA</p>
+      <p>© 2026 - Este site é um projeto independente para fins educacionais e não possui vínculo oficial com o SENAI</p>
     </footer>
   );
 }
